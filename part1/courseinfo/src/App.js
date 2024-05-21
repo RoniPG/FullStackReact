86948a4bc36ba278bfbee39f6311d625b0f01322
RoniPG <ronigmn@gmail.com>
2024-05-21 17:29:19 +0200

    Task 2.18

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,18 +2,51 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 const Countries = ({countries, filteredCountries}) => {
+  const languages = filteredCountries.map((country)=> country.languages) 
   if (filteredCountries.length ===0 || filteredCountries.length > 10) {
     return <p>Too many matches, specify another filter</p>
   } else if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
     return (
-      filteredCountries.map((country) => {
+      filteredCountries.map((country, index) => {
         return (
+          <div key={index}>
         <p>{country.name.common}</p>
+        </div>
         )
       })
     )
+  }else if (filteredCountries.length === 1) {
+    return (
+      <div>
+      <h1>{filteredCountries[0].name.common}</h1>
+      <p>capital {filteredCountries[0].capital}</p>
+      <p>area {filteredCountries[0].area}</p>
+      <p><strong>languages:</strong></p>
+      <ul>
+        {/* Con componente...
+         <Languages languages={languages}/> */}
+        { // Sin componente
+        Object.values(languages[0])
+        .map(language => <li>{language}</li>)
+        }
+      </ul>
+      <img alt={filteredCountries[0].flags.alt} src={filteredCountries[0].flags.png}/>
+      </div>
+    )
   }
 }
+// Versión con un nuevo componente
+// const Languages = ({languages}) => {
+//    const languagesResult = Object.values(languages[0])
+//    console.log("languagesResult: ", languagesResult);
+//   return (
+//     <div>
+//       { 
+//       languagesResult.map(language => <li>{language}</li>)
+//       }
+//     </div>
+//   )
+// }
 const App = () => {
   console.log("render APP");
 
@@ -30,11 +63,10 @@ const App = () => {
   }, []) 
   const handleChange = (event) =>{
     setFiter(event.target.value)
-    const findCountries = countries.map((country) => {
-      return country.name.common.contains(event.target.value)}
-    )
+    const findCountries = countries.filter((country) => 
+      country.name.common.toLowerCase().includes(event.target.value.toLowerCase()))
     console.log(findCountries);
-    //setfilteredCountries(countries.find((country) => country.name.common.some(event.target.value)))
+    setfilteredCountries(findCountries)
   }
 
   return (
