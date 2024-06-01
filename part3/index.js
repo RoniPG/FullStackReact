1ec3134c54b7a17ae4bde2091febb8311172f933
RoniPG <ronigmn@gmail.com>
2024-06-01 13:26:05 +0200

    Subiendo el build, cambiando a ruta relativa, copiando el build al backend

--- part3/index.js
+++ part3/index.js
@@ -2,8 +2,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan("tiny"))
